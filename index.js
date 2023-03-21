d687364a0851c330ec3fe79f55e86013de58f2ab
Ikhor03 <imamkhoiri725@gmail.com>
2023-03-21 22:18:13 +0700

    fix crash

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.use((req, res) => {
 
 
 
-app.listen(5000, () => console.log('server up and running in port 5000'))
+app.listen(process.env.PORT ||  5000, () => console.log('server up and running in port 5000'))
 
 module.exports = app
